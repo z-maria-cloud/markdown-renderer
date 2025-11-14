797588f1bcd3eeec2a763129d900ec37e0b31566
z-maria-cloud <197367564+z-maria-cloud@users.noreply.github.com>
2025-11-14 23:38:08 +0100

    added nonexistent file handler

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Markdown Render
+# Markdown Renderer
 
 This website renders Markdown documents as webpages.
+
+## homepage.md
+
+This will become the website's homepage.
--- index.js
+++ index.js
@@ -27,6 +27,21 @@ app.get("/", (req, res) => {
   res.render("main.ejs", { data: homepageRender });
 });
 
+app.get("/page/:pageName", (req, res) => {
+  let requestedPage = req.params.pageName;
+  let responseView, page, resData;
+  try {
+    page = fs.readFileSync("./documents/`${}`.md", {
+      encoding: "utf8",
+      flag: "r",
+    });
+    pageRender = md.render(page);
+    res.render("main.ejs", { data: pageRender });
+  } catch {
+    res.render("error.ejs");
+  }
+});
+
 app.listen(port, () => {
   if (process.env.NODE_ENV === "production") {
     console.log("SMILE! you're in production");
